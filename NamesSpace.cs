4689adf724402d54a31119fc39942ba6f24c8e30
endrewazevedo <maycon.expert21@gmail.com>
2021-12-20 12:01:33 -0400

    Adiciona pessoa

---
+++ NamesSpace.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pessoas{
+    class Pessoa{
+        public string? Nome { get; set;}
+        public int Idade {get; set;}
+        public string? Profissao { get; set; } 
+    }
+}
---
+++ mostrar_pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pessoas;
+
+class program{
+    static void Main(string[] args){
+    Pessoa teste = new Pessoa();
+    teste.Nome = "Maycon";
+    teste.Idade = 22;
+    teste.Profissao = "Estagiário";
+    Console.WriteLine(teste.Nome);
+    Console.WriteLine(teste.Idade);
+    Console.WriteLine(teste.Profissao);
+}
+}
